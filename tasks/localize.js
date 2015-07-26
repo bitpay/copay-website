a17e2f2327108c0eec8a45c201710e0aeb43dc28
Jason Dreyzehner <jason@bitpay.com>
2015-07-26 11:59:37 -0400

    move to gulp-l10n, use hrefRewrite

--- tasks/localize.js
+++ tasks/localize.js
@@ -5,13 +5,13 @@ var $ = require('gulp-load-plugins')();
 
 gulp.task('extract-locales', function () {
   return gulp.src('dist/**/*.html')
-    .pipe($.s18n.extract())
+    .pipe($.l10n.extract())
     .pipe(gulp.dest('locales'));
 });
 
 gulp.task('load-locales', ['extract-locales'], function () {
   return gulp.src('locales/*.json')
-    .pipe($.s18n.setLocales({
+    .pipe($.l10n.setLocales({
       native: 'en',
       enforce: 'warn'
     }));
@@ -19,6 +19,14 @@ gulp.task('load-locales', ['extract-locales'], function () {
 
 gulp.task('localize', ['load-locales'], function () {
   return gulp.src('dist/**/*.html')
-    .pipe($.s18n())
+    .pipe($.l10n({
+      hrefRewrite: function(href, locale) {
+      if (href.indexOf(':') === -1 && href.charAt(0) === '/') {
+        return '/' + locale + href;
+      } else {
+        return href;
+      }
+    }
+    }))
     .pipe(gulp.dest('dist'));
 });
